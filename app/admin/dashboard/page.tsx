bba4e99741a149d4c20f12fa2c7dd23fbcade619
Arthath Agary <arthathagary@gmail.com>
2024-08-31 12:49:29 +0530

    add admin uis

---
+++ app/admin/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import { Dashboard } from "@/components/admin/dashboard";
+import React from "react";
+
+const AdminDashboardPage = () => {
+  return <Dashboard />;
+};
+
+export default AdminDashboardPage;
---
+++ app/admin/page.tsx
@@ -0,0 +1,8 @@
+import { Login } from "@/components/admin/login";
+import React from "react";
+
+const AdminPage = () => {
+  return <Login />;
+};
+
+export default AdminPage;
